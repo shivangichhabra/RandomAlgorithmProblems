98688201b66e0dd8ceae51c04c037ef12b98b2d7
Shivangi Chhabra <shivangi.chhabra@cerner.com>
2017-06-13 14:50:07 -0500

    Added Clock Angle and Running Median problem

---
+++ src/CalculateClockAngle.java
@@ -0,0 +1,31 @@
+/**
+ * Created by FS047207 on 6/6/2017.
+ */
+public class CalculateClockAngle {
+
+    public double getAngle(int hour, int minute){
+        System.out.println(hour + "  " + minute);
+        if(hour < 0 || minute < 0)
+            return -1;
+
+        if(hour == 12)
+            hour = 0;
+
+        if(minute == 60){
+            minute = 0;
+            hour += 1;
+        }
+        
+        double hourAngle = 0.5 * (60*hour + minute);  //angle for hour wand
+        double minAngle = 6 * minute; //angle for minute wand
+        double angle = Math.abs(hourAngle-minAngle);
+        return Math.min(angle, 360-angle);
+    }
+
+    public static void main(String args[]){
+        CalculateClockAngle c = new CalculateClockAngle();
+        String time = "2:23";
+        String[] values = time.split(":");
+        System.out.println(c.getAngle(Integer.parseInt(values[0]), Integer.parseInt(values[1])));
+    }
+}
---
+++ src/FindRunningMedian.java
@@ -0,0 +1,40 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+/**
+ * Created by FS047207 on 6/6/2017.
+ */
+public class FindRunningMedian {
+    PriorityQueue<Integer> minHeap;
+    PriorityQueue<Integer> maxHeap;
+
+    public FindRunningMedian(){
+        minHeap = new PriorityQueue<>();
+        maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+    }
+
+    public void addNum(int num){
+        maxHeap.offer(num);
+        minHeap.offer(maxHeap.poll());
+
+        if(maxHeap.size() < minHeap.size())
+            maxHeap.offer(minHeap.poll());
+    }
+
+    public double findMedian(){
+        if(maxHeap.size() == minHeap.size())
+            return (double)(maxHeap.peek() + minHeap.peek())/2;
+        else
+            return maxHeap.peek();
+    }
+
+    public static void main(String args[]){
+        FindRunningMedian frm = new FindRunningMedian();
+        int[] a = {12,4,5,3,8,7,1};
+
+        for(int i=0; i<a.length; i++){
+            frm.addNum(a[i]);
+            System.out.println(frm.findMedian());
+        }
+    }
+}
