af6a00baebadb6730016a59c98eb198113807f2f
shivangichhabra <keepintouch.shivangi26@gmail.com>
2017-06-27 12:44:12 -0500

    kth smallest in matrix

---
+++ src/KthSmallestElementInSortedMatrix.java
@@ -0,0 +1,46 @@
+/**
+ * Created by keepi on 6/27/2017.
+ */
+public class KthSmallestElementInSortedMatrix {
+
+    public int kthSmallest(int[][] matrix, int k) {
+        int start = matrix[0][0];
+        int end = matrix[matrix.length-1][matrix.length-1];
+
+        while(start < end){
+            int mid = (start+end)/2;
+            int count = getCount(matrix, mid);
+            if(count < k)
+                start = mid+1;
+            else
+                end = mid;
+        }
+        return end;
+    }
+
+
+    public int getCount(int[][] matrix, int mid){
+        int m = matrix.length;
+        int i = m-1;
+        int j = 0;
+        int count = 0;
+
+        while(i>=0 && j<m){
+            if(matrix[i][j] <= mid){
+                count += i+1;
+                j++;
+            } else{
+                i--;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String args[]){
+        KthSmallestElementInSortedMatrix k = new KthSmallestElementInSortedMatrix();
+        int[][] matrix = {{1,5,9},{10,11,13},{12,13,15}};
+        int result = k.kthSmallest(matrix, 13);
+
+        System.out.println(result);
+    }
+}
