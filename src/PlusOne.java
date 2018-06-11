638d3fb71124b26d93cef98c38c79eb3adb26978
Shivangi Chhabra <keepintouch.shivangi26@gmail.com>
2018-06-10 21:44:33 -0500

    updated plus one

--- src/PlusOne.java
+++ src/PlusOne.java
@@ -4,12 +4,26 @@
 public class PlusOne {
     public static void main(String args[]){
         int digits[] = {1,2,3};
-        int n = digits.length;
-        for(int i=n-1; i >= 0; i--){
-            if(digits[i] < 9)
+        plusOne(digits);
+
+        for(int i=0; i<digits.length; i++)
+            System.out.println(digits[i]);
+    }
+
+    public static int[] plusOne(int[] digits) {
+        if(digits.length == 0 || digits == null )
+            return new int[1];
+
+        for(int i=digits.length-1; i>=0; i--){
+            if(digits[i] < 9){
                 digits[i]++;
-            else
-                digits[i] = 0;
+                return digits;
+            }
+
+            digits[i] = 0;
         }
+        int[] newNum = new int[digits.length+1];
+        newNum[0] = 1;
+        return newNum;
     }
 }
